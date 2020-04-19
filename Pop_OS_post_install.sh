5118c7866d411e0b811180fa0768a311f74f7d4a
Ryan Carroll <rcarroll215@gmail.com>
2020-04-18 21:22:01 -0400

    Create Pop_OS_post_install.sh

---
+++ Pop_OS_post_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Install updates
+sudo apt update
+sudo apt upgrade -y
+
+#Add PPAs
+sudo add-apt-repository ppa:papirus/papirus
+
+#Install packages from repos
+sudo apt install -y vim curl powerline gstreamer1.0-libav libthumbnailer0 ffmpegthumbnailer gnome-session tilix gnome-tweak-tool papirus-icon-theme network-manager-openvpn-gnome vlc remmina-plugin-* gimp nmap wireshark ruby screenfetch neofetch msgfmt gettext ninja-build meson sassc docker.io cmatrix papirus-icon-theme
+
+#Install ruby gems
+sudo gem install lolcat
